85076ace230436bc4a588c39234d4428e21883c7
Bluscream <Bluscream@users.noreply.github.com>
2024-03-07 00:50:41 +0100

    a

--- arma2convertmods.sh
+++ arma2convertmods.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-find ./arma3/233780/steamapps/workshop/content/107410/ -depth -name "*[A-Z]*" -print0 |\
+find ./arma3/33905/steamapps/workshop/content/33900/ -depth -name "*[A-Z]*" -print0 |\
  xargs -0 -I {} bash -c "mv \"{}\" \"\`echo \"{}\" | sed 's,\(.*\)\/\(.*\),\1\/\L\2,'\`\""
 exit 0
