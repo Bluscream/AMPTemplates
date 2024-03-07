387a0a9d4efbf3de8148a3f2c00b4b114c941af3
Bluscream <Bluscream@users.noreply.github.com>
2024-03-07 07:48:58 +0100

    aaa

--- arma2oamodscript.sh
+++ arma2oamodscript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-find ./arma2oa/33935/steamapps/workshop/content/33930/ -depth -name "*[A-Z]*" -print0 |\
+find ./arma2oa/33930/steamapps/workshop/content/33930/ -depth -name "*[A-Z]*" -print0 |\
  xargs -0 -I {} bash -c "mv \"{}\" \"\`echo \"{}\" | sed 's,\(.*\)\/\(.*\),\1\/\L\2,'\`\"";\
- cd ./arma2oa/33935 &&\
+ cd ./arma2oa/33930 &&\
  find ./steamapps/workshop/content/33930 -maxdepth 1 -mindepth 1 -type d -exec ln -sf -t ./ {} +
 exit 0
