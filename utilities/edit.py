85a192ee90c477477d910c56bfa6a7050cd33f1f
Bluscream <Bluscream@users.noreply.github.com>
2024-08-17 19:41:45 +0200

    a

--- utilities/edit.py
+++ utilities/edit.py
@@ -1,26 +1,42 @@
 import json
 import codecs
+import os
+from pathlib import Path
 
-filepath = 'operation-harsh-doorstop-devconfig.json'
+def editFile(filepath):
+    with codecs.open(filepath, 'r', 'utf-8-sig') as file:
+        data = json.load(file)
 
-# Open the JSON file with 'utf-8-sig' encoding to remove BOM if present
-with codecs.open(filepath, 'r', 'utf-8-sig') as file:
-    data = json.load(file)
+    print(f"Editing {filepath}")
 
-for i, obj in enumerate(data):
-    # Sort the keys of the current object alphabetically
-    sorted_obj = {k: obj[k] for k in sorted(obj)}
+    key = "MinValue"
+    for i, obj in enumerate(data):
+          if key in obj:
+                if obj[key].isdigit():
+                      obj[key] = int(obj[key])
+        # sorted_obj = {k: obj[k] for k in sorted(obj)}
+        
+        # Update the object in the list with the sorted version
+        # data[i] = sorted_obj
+    # for obj in data:
+    #     if "FieldName" not in obj:
+    #         obj["FieldName"] = obj.get("ParamFieldName").split('.')[-1]
+        # Check if InputType is "checkbox" and EnumValues does not exist
+        # if obj.get("InputType") == "checkbox" and "EnumValues" not in obj:
+        #     # Add EnumValues to the object
+        #     obj["EnumValues"] = {"True": "True", "False": "False"}
+
+    # Save the modified JSON back to the file with 'utf-8' encoding
+    with codecs.open(filepath, 'w', 'utf-8') as file:
+        json.dump(data, file, ensure_ascii=False, indent=4)
     
-    # Update the object in the list with the sorted version
-    data[i] = sorted_obj
-# for obj in data:
-#     if "FieldName" not in obj:
-#         obj["FieldName"] = obj.get("ParamFieldName").split('.')[-1]
-    # Check if InputType is "checkbox" and EnumValues does not exist
-    # if obj.get("InputType") == "checkbox" and "EnumValues" not in obj:
-    #     # Add EnumValues to the object
-    #     obj["EnumValues"] = {"True": "True", "False": "False"}
+    print(f"Edited {filepath}")
 
-# Save the modified JSON back to the file with 'utf-8' encoding
-with codecs.open(filepath, 'w', 'utf-8') as file:
-    json.dump(data, file, ensure_ascii=False, indent=4)
+def editDir(dirpath, filter = ".json"):
+    for root, dirs, files in os.walk(dirpath):
+            for file in files:
+                if file.endswith(filter):
+                            fullpath = Path(root) / file
+                            editFile(fullpath)
+
+editDir('.', '.config.json')
